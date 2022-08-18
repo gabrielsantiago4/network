af87c3a5d32ee866c2e83bbe0550f5d2f1d20f67
Gabriel Santiago <gaaasts@gmail.com>
2022-08-18 09:13:43 -0300

    first commit

---
+++ Network/Network/NetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkApp.swift
+//  Network
+//
+//  Created by Gabriel Santiago on 18/08/22.
+//
+
+import SwiftUI
+
+@main
+struct NetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
